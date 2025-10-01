b5fa5c6254dc804b551b5a648f0c67adfb60b4bf
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-10-01 18:42:49 +0600

    variables

---
+++ 2-variables/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>2-variables</title>
+</head>
+<body>
+    <h1>
+        <?php
+            $greeting = "Hello";
+            $name = "Tahsin";
+            echo "$greeting $name <br>"; // Interpolation
+            echo "Hello" . " " . "Tahsin"; // Concatenation
+        ?>
+    </h1>
+</body>
+</html>
