347ed5c3de37339a0cc8c99cc609e2cdaa78eb92
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-08-13 18:44:27 +0600

    manage password

---
+++ 35-manage-password/controllers/registration/store.php
@@ -0,0 +1,47 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$errors = [];
+
+if (!Validator::email($email)) {
+    $errors['email'] = "Please provide a valid email address.";
+}
+
+if (!Validator::string($password, 7, 255)) {
+    $errors['password'] = "Please provide a password of at least 7 characters.";
+}
+
+if (!empty($errors)) {
+    return view('registration/create.view.php', [
+        'errors' => $errors
+    ]);
+}
+
+$db = App::resolve(Database::class);
+
+$user = $db->query("select * from users where email = :email", [
+    'email' => $email
+])->find();
+
+if ($user) {
+    header("Location: /");
+    exit();
+}
+
+$db->query("insert into users (email, password) values (:email, :password)", [
+    'email' => $email,
+    'password' => password_hash($password, PASSWORD_DEFAULT)
+]);
+
+$_SESSION['user'] = [
+    'email' => $email,
+];
+
+header("Location: /");
+exit();
