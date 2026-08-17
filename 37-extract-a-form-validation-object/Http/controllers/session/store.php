9db8f3c8fdb68dfbb9eee06fa1d8a2622518a5ef
Tahsin005 <tahsin.ferdous3546@gmail.com>
2026-08-17 22:38:06 +0600

    extract a form validation object

---
+++ 37-extract-a-form-validation-object/Http/controllers/session/store.php
@@ -0,0 +1,40 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+use Http\Forms\LoginForm;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$form = new LoginForm();
+
+if (!$form->validate($email, $password)) {
+
+    return view('session/create.view.php', [
+        'errors' => $form->errors()
+    ]);
+}
+
+$db = App::resolve(Database::class);
+
+$user = $db->query("select * from users where email = :email", [
+    'email' => $email
+])->find();
+
+if ($user) {
+    if (password_verify($password, $user['password'])) {
+        login([
+            'email' => $email,
+        ]);
+        header("Location: /");
+        exit();
+    }
+}
+
+return view("session/create.view.php", [
+    'errors' => [
+        "email" => "Invaild email or password."
+    ]
+]);
